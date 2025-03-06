7fa416df16d339f345582c6c681aecb9b343bae9
Jade Cong <jade.cong@qq.com>
2025-03-06 19:30:59 +0800

    update update-readme python script

--- scripts/update-readme.py
+++ scripts/update-readme.py
@@ -1,5 +1,4 @@
 import os, sys
-import pathlib
 import re
 import datetime
 
@@ -12,28 +11,60 @@ def get_file_names(folder_path):
     
     return file_names
 
-def get_recent_project_list(file_names):
+def fetch_recent_project_list(file_names):
     recent_project_list = []
     recent_project_list = sorted(file_names, reverse=False)[:5]
     
     return recent_project_list
 
-def get_recent_blog_list(file_names):
+def fetch_recent_blog_list(file_names):
     recent_blog_list = []
     recent_blog_list = sorted(file_names, reverse=True)[:5]
     
     return recent_blog_list
 
+def replace_chunk(content, marker, chunk, inline=False):
+    pattern = re.compile(r"<!\-\- {}:Start \-\-!>.*?<!\-\- {}:End \-\-!>".format(marker), re.DOTALL)
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {}:Start -->{}<!-- {}:End -->".format(marker, chunk, marker)
+    
+    return pattern.sub(chunk, content)
+
 
 if __name__ == '__main__':
-    print("Update readme for JadeCong")
+    print("---------- Update readme for JadeCong ----------")
     
     # Get recent project list from target repository
     project_path = "JadeCong.github.io/_projects"
-    recent_projects = get_recent_project_list(get_file_names(project_path))
+    recent_projects = fetch_recent_project_list(get_file_names(project_path))
     print(recent_projects)
     
     # Get recent blog list from target repository
     blog_path = "JadeCong.github.io/contents/blogs/_posts"
-    recent_blogs = get_recent_blog_list(get_file_names(blog_path))
+    recent_blogs = fetch_recent_blog_list(get_file_names(blog_path))
     print(recent_blogs)
+    
+    # Update recent project and blog list in README.md
+    with open("README.md", 'r', encoding='utf-8') as file:
+        readme_content = file.read()
+    print("Readme Content:\n", readme_content)
+    ## Update recent project list
+    recent_projects_chunk = "".join(
+        [
+            "- [{}](https://jadecong.github.io/contents/projects/{}/)".format(project.split('.')[0].replace("-", " "), project.split('.')[0])
+            for project in recent_projects
+        ]
+    )
+    readme_content = replace_chunk(readme_content, "Recent-Project-List", recent_projects_chunk)
+    ## Update recent blog list
+    recent_blogs_chunk = "".join(
+        [
+            "- [{}](https://jadecong.github.io/contents/blogs/{}/)".format(blog.split('.')[0][11:].replace("-", " "), blog.split('.')[0])
+            for blog in recent_blogs
+        ]
+    )
+    readme_content = replace_chunk(readme_content, "Recent-Blog-List", recent_blogs_chunk)
+    print("Updated Readme Content:\n", readme_content)
+    with open("README.md", 'w', encoding='utf-8') as file:
+        file.write(readme_content)
